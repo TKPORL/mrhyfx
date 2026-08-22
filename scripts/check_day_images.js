cd0b53ebf421fd74443c3cfdb3f1f13df166c283
Tsinho <tsinho@example.com>
2026-08-22 16:01:10 +0800

    chore: remove temp check scripts

--- scripts/check_day_images.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('8.21PC.html', 'utf8');
-const imgMatches = html.match(/src="(https:\/\/cdn\.jsdelivr\.net\/gh\/TKPORL\/mrhyfx@main\/assets\/[^"]+)"/g);
-console.log('8.21PC.html image refs:', imgMatches ? imgMatches.length : 0);
-if (imgMatches) console.log(imgMatches.slice(0, 5));
-
-// Also check for any image refs
-const allImgRefs = html.match(/src="[^"]*"/g);
-console.log('All src refs:', allImgRefs ? allImgRefs.length : 0);
